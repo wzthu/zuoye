efc8d460f22f7eefbe3ba7b54ba320c4978d976e
Fengchen <fackie520@126.com>
2018-03-08 16:43:07 +0800

    test_cellranger

--- cellranger.py
+++ cellranger.py
@@ -2,85 +2,72 @@
 # -*- coding: utf-8 -*-
 """
 @author: Frankie
-20180306
+@date: 20180308
 """
-from stepbase import Step,Configure
-import subprocess
+from StepBase import Step
 import os
 
 class Cellranger(Step):
+
     def __init__(self,
-        outputdir = None,
-        fastqInput = None, 
-        refile = None,
-        expectcells = 2000,
-        cmdParam = None,
-        #threads = None,
-        **kwargs):
+                 outputdir = None,
+                 fastqInput = None,
+                 refile = None,
+                 expectcells = 2000,
+                 cmdParam = None,
+                 **kwargs):
         super(Step, self).__init__(cmdParam,**kwargs)
-        self.setParamIO('fastqInput',fastqInput)
-        self.setParamIO('refile',refile)
-        self.setParamIO('outputdir',outputdir)
+        self.setParamIO('fastqInput', fastqInput)
+        self.setParamIO('refile', refile)
+        self.setParamIO('outputdir', outputdir)
+        self.setParam('id', outputdir)
         self.initIO()
-        # self.setPaif threads is None:
-        # threads = Configure.getThreads()
-        # self.setParam('threads',threads)
         self.setParam('expectcells', expectcells)
         self._setMultiRun()
 
-	#  `
-
     def impInitIO(self,):        
         fastqInput = self.getParamIO('fastqInput')
-        #fastqInput2 = self.getParamIO('fastqInput2')
-        refile = self.getParamIO('refile')
         outputdir = self.getParamIO('outputdir')
-        
-        #set all input files
-        
-        self.setInputDirOrFile('fastqInput',fastqInput)
-        #self.setInputDir('fastqInput',fastqInput)
-        self.setInputDirOrFile('refile',refile)  
-        
-        self.setOutDirNTo1('barcodes',os.path.join(outputdir,'outs','filtered_gene_bc_matrices',Configure.getGenome(),'barcode.tsv','fastqInput')
-        self.setOutDirNTo1('genes',os.path.join(outputdir,'outs','filtered_gene_bc_matrices',Configure.getGenome(),'genes.tsv','fastqInput')
-        self.setOutDirNTo1('matrix',os.path.join(outputdir,'outs','filtered_gene_bc_matrices',Configure.getGenome(),'matrix.mtx','fastqInput')
-        
-            
-#    def setOutputDir1To1(self, outputName, outputDir, outputPrefix, outputSuffix, inputName):
-#        # inputList = self.getInput(inputName)
-#        # if inputList is None:
-#            # self.setOutput(outputName, None)
-#        # else:
-#            # if not isinstance(inputList,list):
-#                # inputList = [inputList]
-#            # outputList = []            
-#            # for i in range(len(inputList)):
-#                # outputList.append(outputPrefix + '_' + str(i) + '_' + outputSuffix)
-#            # if outputDir is None:
-#                # self.setOutput(outputName,Configure.getTmpPath(outputList))
-#            # else:        
-#				self.setOutput(outputName,[outputPrefix + '_' + str(i) + '_' + outputSuffix])
-				
-#    def _multiRun(self,):
-#        fastqInput = self.getInputList('fastqInput')
-#        expectcells = self.getParam('expectcells')
-#        refile = self.getInputList('refile')
-#        outputdir = self.getOutputList('outputdir')
-#        cmdline = ['cellranger count',
-#           '--id=%s --expect-cells=%s --transcriptome=%s --fastqs=%s' 
-#           % (outputdir,str(expectcells),refile,fastqInput)]
-#        print(cmdline)
-#        self.callCmdline(cmdline)
-        
+        refile = self.getParamIO('refile')
+
+        self.setInputDirOrFile('fastqInput', fastqInput)
+
+        self.setInputDirOrFile('version', os.path.join(refile, 'version'))
+        self.setInputDirOrFile('Reference', os.path.join(refile, 'reference.json'))
+        self.setInputDirOrFile('README', os.path.join(refile, 'README.BEFORE.MODIFYING'))
+        for i in ['chrLength.txt', 'chrName.txt',
+                  'exonGeTrInfo.tab', 'geneInfo.tab',
+                  'genomeParameters.txt', 'SAindex',
+                  'sjdbList.fromGTF.out.tab', 'transcriptInfo.tab',
+                  'chrNameLength.txt', 'chrStart.txt',
+                  'exonInfo.tab', 'Genome', 'SA',
+                  'sjdbInfo.txt', 'sjdbList.out.tab']:
+            self.setInputDirOrFile(i, os.path.join(refile, 'star', i))
+        self.setInputDirOrFile('genes.pickle', os.path.join(refile, 'pickle', 'genes.pickle'))
+        self.setInputDirOrFile('genes.gtf', os.path.join(refile, 'genes', 'genes.gtf'))
+        self.setInputDirOrFile('genome.fa', os.path.join(refile, 'fasta', 'genome.fa'))
+
+        self.setOutputDirNTo1('barcodes', os.path.join(outputdir, 'outs', 'filtered_gene_bc_matrices', 'barcode.tsv'), '', 'fastqInput')
+        self.setOutputDirNTo1('genes', os.path.join(outputdir, 'outs', 'filtered_gene_bc_matrices', 'genes.tsv'), '', 'fastqInput')
+        self.setOutputDirNTo1('matrix', os.path.join(outputdir, 'outs', 'filtered_gene_bc_matrices', 'matrix.mtx'), '', 'fastqInput')
+
     def call(self, *args):
-        pass #print(self.cmdline)          
-	
-    def _singleRun(self, i):
-        fastqInput = self.getInput('fastqInput')
+        pass
+        # print(self.cmdline)
+
+    def _multiRun(self, ):
+        fastqInput = self.getParamIO('fastqInput')
+        refile = self.getParamIO('refile')
+        id = self.getParam('id')
+        outputdir = self.getParamIO('outputdir')
         expectcells = self.getParam('expectcells')
-        refile = self.getInput('refile')#getParamIO('refile')
-        outpout = self.getParamIO('refile')
-        self.cmdline = ["cellranger count --id=%s --expect-cells=%s --transcriptome=%s --fastqs=%s" % (output[i],str(expectcells),refile[i],fastqInput[i])]
-        print(self.cmdline[0])
-        self.callCmdline(self.cmdline[0])
+
+        # if same outputdir already exist, delete it
+        if os.path.isdir(outputdir):
+            self.cmdline1 = ['rm', '-r %s' % outputdir]
+            self.callCmdline(self.cmdline1)
+        # run 10x cellranger
+        self.cmdline2 = ['cellranger', 'count', '--id=%s --expect-cells=%s --transcriptome=%s --fastqs=%s'
+                         % (id, str(expectcells), refile, fastqInput)]
+        self.callCmdline(self.cmdline2)
+
--- test.py
+++ test.py
@@ -1,10 +1,8 @@
 from cellranger import Cellranger
-from stepbase import Configure,Schedule
+from StepBase import Configure, Schedule
 
-Configure.setRefDir('/home/zwei/ref')
-Configure.setGenome('hg19')
 
-test = Cellranger(fastqInput = '/home/cfeng/data/fastqs/', outputdir='test_cellranger', refile = '/home/cfeng/data/refdata-cellranger-hg19_and_mm10-1.2.0', expectcells=100)
+test = Cellranger(fastqInput = '/home/cfeng/data/test/', outputdir='test_cellranger', refile = '/home/cfeng/data/refdata-cellranger-hg19_and_mm10-1.2.0', expectcells=100)
 Schedule.run()
 
 print('')
