3e8bb09ec642be0e03bfa3b2b9f0906f9641fbdd
ZweiTHU <zwfanwz678@gmail.com>
2018-03-09 21:02:55 +0800

    add test file

--- test_AdapterRemoval.py
+++ test_AdapterRemoval.py
@@ -1,15 +1,12 @@
+# -*- coding: utf-8 -*-
 from Bowtie2 import Bowtie2
 from AdapterRemoval import AdapterRemoval
 from StepBase import Configure,Schedule
 
-import os
-
 Configure.setRefDir('/home/wzhang/genome/hg19_bowtie2/')
 Configure.setGenome('hg19')
 
-rs=Bowtie2(fastqInput1='./minidata/atac/end1',
-           fastqInput2='./minidata/atac/end2')
+
+adrm = AdapterRemoval(fastqInput1='./minidata/atac/end1',fastqInput2='./minidata/atac/end2')
 
 Schedule.run()
-
-
