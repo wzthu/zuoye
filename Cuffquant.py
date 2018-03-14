09565c75fc3d76fceafa93b789175cacb7a00b83
Shengquan Chen <342663654@qq.com>
2018-03-14 20:46:17 +0800

    whole pipeline of smartseq is finished

--- Cuffquant.py
+++ Cuffquant.py
@@ -4,7 +4,7 @@ Created on 2018-3-10 09:24:38
 @author: Song Shaoming
 """
 
-from stepbase import Step,Configure
+from StepBase import Step,Configure
 import os
 
 class Cuffquant(Step):
@@ -33,11 +33,20 @@ class Cuffquant(Step):
 		bamInput = self.getParamIO('bamInput')
 		gtfInput = self.getParamIO('gtfInput')
 		outputDir = self.getParamIO('outputDir')
+		if outputDir is None:
+			self.setParamIO('outputDir',Configure.getTmpDir())
+
+		if gtfInput is None:
+			gtfInput=Configure.getConfig('')
+			self.setIput('gtfInput',gtfInput)
+			self.setParamIO('gtfInput',gtfInput)
+		else:
+			self.setInput('gtfInput',gtfInput)
 
 		self.setInputDirOrFile('bamInput',bamInput)
 
 		#self.setOutputDir1To1('outputDir',outputDir,'cuffquant','suffix','bamInput')
-		self.setOutput('assembliesOutput',os.path.join(Configure.getTmpDir(), 'assembleOfAbundances.txt'))
+		#self.setOutput('assembliesOutput',os.path.join(Configure.getTmpDir(), 'assembleOfAbundances.txt'))
 
 		if bamInput is not None:
 			self._setInputSize(len(self.getInputList('bamInput')))
@@ -61,12 +70,9 @@ class Cuffquant(Step):
 
 		cmdline = [
 				'cuffquant',
-				'-o',os.path.join(outputDir,'cuffquant_' + str(i))
+				'-o',os.path.join(outputDir,'cuffquant_' + str(i)),
 				'-p',str(self.getParam('threads')),
 				gtfInput,
-				bamInput[i],
-				';',
-				'echo','"'+os.path.join(outputDir,'cuffquant_'+str(i),'abundances.cxb') + '" >>',
-				self.getOutput("assembliesOutput")
+				bamInput[i]
 				]
-		self.callCmdline(cmdline)
+		self.callCmdline('V1',cmdline)
