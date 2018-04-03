892ea50061a13a47f23babd396287635ef6825ba
Fengchen <fackie520@126.com>
2018-04-03 11:59:46 +0800

    Flowupdated (#79)

--- hcacn/flows/__init__.py
+++ hcacn/flows/__init__.py
@@ -4,3 +4,4 @@ from .FlowSmartseq_2 import FlowSmartseq_2
 from .MatrixPreprocess import MatrixPreprocess
 from .MonocleDimentionReductionAndClusterFlow import MonocleDimentionReductionAndClusterFlow
 from .DropseqFlow import DropseqFlow
+from .FlowCellranger import FlowCellranger
