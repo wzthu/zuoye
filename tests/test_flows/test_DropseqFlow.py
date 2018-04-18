8c8f7a38f7ba2354d12081cf88c8cdb0232a4ae6
bhcs96 <36975365+bhcs96@users.noreply.github.com>
2018-04-18 10:19:46 +0800

    DropseqFlow Add Param (#88)

--- tests/test_flows/test_DropseqFlow.py
+++ tests/test_flows/test_DropseqFlow.py
@@ -3,5 +3,5 @@ from hcacn.core import Configure,Schedule
 from hcacn.flows import DropseqFlow
 Configure.setIdentity('lcy')
 
-df=DropseqFlow(fastqInput1='./minidata/dropseq/read1', fastqInput2='./minidata/dropseq/read2',refDir=None, refdir='/data8t_1/ref/dropseq/', genome='hg19-and-mm10')()
+df=DropseqFlow(fastqInput1='./minidata/dropseq/read1', fastqInput2='./minidata/dropseq/read2',refInput='/data8t_1/ref/dropseq/hg19-and-mm10_refdata-cellranger-2.1.0', refdir='/data8t_1/ref/dropseq/', genome='hg19-and-mm10', expectedCellNum=20000)()
 
